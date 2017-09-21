104bdca90f55f6c0f48d0ac24fc4a899a82e8319
Dave Xiong <xionghaitao.org@gmail.com>
2017-09-21 16:20:31 +1000

    format

--- sonarqube-sample-java/src/test/java/com/consoleconnect/mw/sonarqube/sample/test/UserTest.java
+++ sonarqube-sample-java/src/test/java/com/consoleconnect/mw/sonarqube/sample/test/UserTest.java
@@ -1,36 +1,37 @@
 /**
  * 
  */
+
 package com.consoleconnect.mw.sonarqube.sample.test;
+
+import com.consoleconnect.mw.sonarqube.sample.entity.User;
 
 import java.util.UUID;
 
 import org.junit.Assert;
 import org.junit.Test;
 
-import com.consoleconnect.mw.sonarqube.sample.entity.User;
 
 /**
+ * 
  * @author dxiong
  *
  */
 
 public class UserTest {
 
-	private static final String ID = UUID.randomUUID().toString();
-	private static User USER = new User(ID);
-	
-	
+  private static final String ID = UUID.randomUUID().toString();
+  private static User USER = new User(ID);
 
-	@Test
-	public void testId() {
-		Assert.assertTrue(USER.getId().equals(ID));
-	}
+  @Test
+  public void testId() {
+    Assert.assertTrue(USER.getId().equals(ID));
+  }
 
-	@Test
-	public void testEmail() {
-		Assert.assertNull(USER.getEmail());
-		USER.setEmail("test@email.com");
-		Assert.assertTrue(USER.getEmail().equals("test@email.com"));
-	}
+  @Test
+  public void testEmail() {
+    Assert.assertNull(USER.getEmail());
+    USER.setEmail("test@email.com");
+    Assert.assertTrue(USER.getEmail().equals("test@email.com"));
+  }
 }
