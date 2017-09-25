7568b1245dde4139d2769583967f84147b621ddc
Dave Xiong <xionghaitao.org@gmail.com>
2017-09-25 10:53:13 +1000

    sync

---
+++ sonarqube-sample-java/src/main/java/com/consoleconnect/mw/sonarqube/sample/entity/Company.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.consoleconnect.mw.sonarqube.sample.entity;
+
+/**
+ * @author dxiong
+ *
+ */
+public class Company {
+
+  private String name;
+
+  /**
+   * get company name
+   * @return name
+   */
+  public String getName() {
+    
+    boolean flag = false;
+    while(flag) {
+      System.out.println("hello");
+    }
+    return name;
+  }
+}
