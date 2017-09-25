f447968713cff40391a999b6f22ac2d95b01853c
Dave Xiong <xionghaitao.org@gmail.com>
2017-09-25 10:54:35 +1000

    add new class student

---
+++ sonarqube-sample-java/src/main/java/com/consoleconnect/mw/sonarqube/sample/entity/Student.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.consoleconnect.mw.sonarqube.sample.entity;
+
+/**
+ * @author dxiong
+ *
+ */
+public class Student {
+
+  
+  public String name() {
+    boolean flag = false;
+    while(flag) {
+      System.out.println("hello");
+    }
+    return"name";
+  }
+}
